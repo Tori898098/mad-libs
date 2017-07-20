5442f0b2840f9ca390bcb3268da5b1fd9e43bd38
Tori898098 <twebbe2677@gmail.com>
2017-07-20 10:49:39 -0400

    Add files via upload

---
+++ libs.js
@@ -0,0 +1,22 @@
+function addlib() {
+
+ var thing1= document.getElementById("thingtext").value;
+document.getElementById("thing").innerHTML=thing1;
+
+var noun1= document.getElementById("nountext").value;
+document.getElementById("noun").innerHTML=noun1;
+
+var sweet1= document.getElementById("sweettext").value;
+document.getElementById("sweet").innerHTML=sweet1;
+
+       }
+
+//     }
+// function addlib4() {
+//
+//
+//       }
+//
+//
+// document.getElementById("thing").innerHTML=thing;
+// document.getElementById("sweet").innerHTML=sweet;
